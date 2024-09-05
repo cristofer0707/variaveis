31164b8f7a6813cfd1c5c207e1fe5b67db5b02bb
CRISTOFER SANTOS <35421697-cristofermota@users.noreply.replit.com>
2024-09-05 01:22:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let anoAtual= 2024;
+const nomeCompleto = "cristofer santos da mota";
+let idade = 16;
+const cusrsoFaculdade = "advocacia";
+let cidadeAtual = "sjp";
+
+console.log(`ola eu sou o ${nomeCompleto} tenho ${idade} anos e estou cusrsando ${cusrsoFaculdade}`);
+
+anoAtual = 2025;
+idade =17;
+cursoFaculdade = "formado em advocacia";
+cidadeAtual = "sjp";
+console.log(`ola tenho ${idade} anos e estou ${cusrsoFaculdade} e mori`);
